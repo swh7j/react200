88f3e2a9566e224eaed64e26343ec8c0ad8158b7
swh_7 <swh_77@naver.com>
2022-08-10 23:48:19 +0900

    챕터 1

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ import './App.css';
 //import ArrowFunc from './practice/R013_ArrowFunction'
 //import ForEach from './practice/R014_ForEach'
 //import Map from './practice/R015_Map'
-import Jquery from './practice/R016_Jquery'
+//import Jquery from './practice/R016_Jquery'
+//import Props from './practice/R017_Props'
+//import PropsDatatype from './practice/R018_PropsDatatype'
+//import PropsBoolean from './practice/R019_PropsBoolean'
+//import PropsObjVal from './practice/R020_PropsObjVal'
+//import PropsRequired from './practice/R021_PropsRequired'
+//import PropsDefault from './practice/R022_PropsDefault'
+//import PropsNodes from './practice/R023_PropsNode'
+//import ReactState from './practice/R024_ReactState'
+//import SetState from './practice/R025_SetState'
+//import ForceUpdate from './practice/R026_ForceUpdate'
+import ComponentClass from './practice/R027_ComponentClass'
 
 class App extends Component {
     render() {
@@ -22,9 +33,7 @@ class App extends Component {
                <h1>Start React 200!</h1>
                <p>HTML 적용</p>
                <p>CSS 적용</p>
-               <Jquery/>
-
-
+               <ComponentClass/>
 
             </div>
         );
